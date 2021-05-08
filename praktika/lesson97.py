621dcacdaa0980f542d9ff6fc73cbe286e63c424
kachim <kachim30@gmail.com>
2021-05-08 19:43:52 +0400

    внес изменения с добовлением списка добуска к администрированию

--- praktika/lesson97.py
+++ praktika/lesson97.py
@@ -1,27 +1,37 @@
 from lesson93 import User
 
+
+
+
 """Создаем администратара на базе класса User с добавлением новых функций"""
 class Admin(User):
 
-    def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age, ):
         super().__init__(first_name, last_name, age)
         self.administrator_Prov = []
 
+    def user_info(self):
+        print(self.last_name + ' ' + self.first_name + ' ' + str(self.age) + "\nСписок привелегий:  " + str(self.administrator_Prov))
 
-    def prov(self, popa):
-        self.administrator_Prov += popa
+    def list_privileges(self, list_p):
+        for x in list_p:
+            self.administrator_Prov.append(x)
 
-    def show_privileges(self):
+    def print_privileges(self):
         print(self.administrator_Prov)
 
+
+
 es = ['разрешено добавлять сообщения', 'разрешено удалять пользователей', 'разрешено банить пользователей' ]
-admin_Bil = Admin("Ilya", 'veretsenntikau', 34)
-admin_Bil.prov(es)
-
-admin_Bil.user_info()
-admin_Bil.show_privileges()
 
 
 
 
+
+
+er = ('разрешено добавлять сообщения', 'разрешено удалять пользователей', 'разрешено банить пользователей')
+
+admin_gom = Admin("Gom", "vert", 32)
+admin_gom.list_privileges(es)
+admin_gom.user_info()
 
