a344ce3b6eb64391991ae8ff8349bb9a287b1a15
kachim <kachim30@gmail.com>
2021-05-09 00:17:47 +0400

    Третья  рабочая версия ElectricCar

--- praktika/lesson99.py
+++ praktika/lesson99.py
@@ -1,30 +1,6 @@
 from lesson98 import Battery
+from lesson98Car import Car
 
-class Car():
-    """Простая модель автомобиля."""
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name.title()
-
-    def read_odometer(self):
-        print("This car has " + str(self.odometer_reading) + " miles on it.")
-
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
-
-    def info_car(self):
-        print(self.get_descriptive_name() + ' ')
 
 
 class ElectricCar(Car):
