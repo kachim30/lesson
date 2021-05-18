c8843661af7829b037865805c12279d792ac5fca
kachim30 <kachim30@gmail.com>
2021-05-19 01:01:09 +0300

    Add files via upload

---
+++ praktika/lesson10_8.py
@@ -0,0 +1,23 @@
+
+"""файлы к lesson идут 'cats.txt', 'dogs.txt' """
+
+def count_words(filename):
+    """Вывод текста файлов"""
+    try:
+        with open(filename) as x:
+            contents = x.read()
+            print(contents.rstrip())
+    except FileNotFoundError:
+        msg = 'Вы неправильно указали файл для открытия'
+        print(msg)
+
+
+x = True
+
+while x:
+    """зацикливания ввода адресов файлов"""
+    ip = input('Адрес файла для открытия:\n')
+    if ip == 'q':
+        break
+    else:
+        count_words(ip)
